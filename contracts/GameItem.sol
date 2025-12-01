6e80abeba4be937fc0e6b83457185821a2ce616c
oyingrace <oyingrace307@gmail.com>
2025-12-01 12:44:15 +0100

    Import OpenZeppelin contracts for ERC721 functionality, access control, and security features in GameItem.sol, enhancing contract capabilities and modularity.

--- contracts/GameItem.sol
+++ contracts/GameItem.sol
@@ -10,3 +10,8 @@ pragma solidity ^0.8.19;
 * - Pausable for emergency control
 */
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
