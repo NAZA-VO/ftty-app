852bb70b19d78f73b742b2456ba75ed415f84741
Victory Odah <vickiedaniels500@gmail.com>
2025-12-27 11:27:07 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
+import FeaturedAssets from '../components/FeaturedAssets';
 import Chains from '@/components/Chains';
 import Community from '../components/Community';
 import Roadmap from '../components/Roadmap';
