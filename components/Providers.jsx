c6c733f4d386bb401c870e89c865a49968b4aa69
oyingrace <oyingrace307@gmail.com>
2025-12-27 09:46:43 +0100

    Remove unused AppKit and WagmiAdapter configurations from Providers component for cleaner code.

--- components/Providers.jsx
+++ components/Providers.jsx
@@ -1,22 +1,5 @@
 "use client";
 
-import { createAppKit } from "@reown/appkit/react";
-import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { mainnet } from "@reown/appkit/networks"; // or other networks
-
-// Create adapter
-const wagmiAdapter = new WagmiAdapter({
-  networks: [mainnet],
-  projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
-});
-
-// Create appkit instance
-const appKit = createAppKit({
-  adapters: [wagmiAdapter],
-  networks: [mainnet],
-  projectId: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID,
-});
-
 export default function Providers({ children }) {
   return <>{children}</>;
 }
